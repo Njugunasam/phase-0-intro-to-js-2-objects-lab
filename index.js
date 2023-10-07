81239ff7b56e6b93df36f42419c11f1f33f1bfe4
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-07 20:24:03 +0300

    solve errors

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+const employee = {
+    name: 'sam',
+    age: 20,
+    jobTitle: 'Developer',
+  };
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+  
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+  }
+  
+  
