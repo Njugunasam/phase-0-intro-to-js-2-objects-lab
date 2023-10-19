fc51d95c5d5884ea34d43e3a98f4e49e0d5c13e7
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-19 20:57:10 +0300

    Functiotodeleteakeyoftheemployee

--- index.js
+++ index.js
@@ -14,3 +14,36 @@ function deleteFromEmployeeByKey(employee, key) {
   delete newEmployee[key];
   return newEmployee;
 }
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+describe('employees', function() {
+  it('updateEmployeeWithKeyAndValue(employee, key, value)', function() {
+    const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'salary', 50000);
+    assert.deepEqual(updatedEmployee, {
+      name: "John",
+      age: 30,
+      role: "Engineer",
+      salary: 50000,
+    });
+  });
+
+  it('destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)', function() {
+    destructivelyUpdateEmployeeWithKeyAndValue(employee, 'salary', 50000);
+    assert.strictEqual(employee.salary, 50000);
+  });
+
+  it('deleteFromEmployeeByKey(employee, key)', function() {
+    const newEmployee = deleteFromEmployeeByKey(employee, 'age');
+    assert.deepEqual(newEmployee, {
+      name: "John",
+      role: "Engineer",
+    });
+  });
+
+  it('destructivelyDeleteFromEmployeeByKey(employee, key)', function() {
+    destructivelyDeleteFromEmployeeByKey(employee, 'role');
+    assert.strictEqual(employee.role, undefined);
+  });
+});
