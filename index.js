3203b595bc3e83e6b38a58ad4569abe512f1b17f
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-19 20:47:39 +0300

    adding a function updateemplpoyeewithkeyandvalue

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// Write your solution in this file!
 const employee = {
-    name: 'sam',
-    age: 20,
-    jobTitle: 'Developer',
-  };
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-  
-  }
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-  }
-  function deleteFromEmployeeByKey(employee, key) {
-  }
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
-
-  }
-  
-  
+  name: "John",
+  age: 30,
+  role: "Engineer",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+ 
+  const updatedEmployee = { ...employee };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+}
